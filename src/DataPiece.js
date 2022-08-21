fa02e29f117c80a1037374714c373f9c16b71be2
Maciej Fec <maciejfec1996@gmail.com>
2022-08-21 14:08:27 +0100

    adjust height of data pieces

--- src/DataPiece.js
+++ src/DataPiece.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function DataPiece({dataValue}) {
 
-  const height = dataValue.value * 10
+  const height = dataValue.value * 17
   
   return (
     <div className="data-piece" style={{"height": `${height}px`,"backgroundColor": dataValue.colour}}></div>
