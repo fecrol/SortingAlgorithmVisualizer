4d51428ccb13fcda7864fe8f8f43438ab2ed2e62
Maciej Fec <maciejfec1996@gmail.com>
2022-08-21 14:07:24 +0100

    Add sorting algorithms

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import "./App.css"
 function App() {
   
   const [data, setData] = useState([])
-  const [sorted, setSorted] = useState(false)
+  const [isSorted, setIsSorted] = useState(false)
 
-  const defaultColour = "#03a9f4"
-  const currentItemColour = "red"
-  const itemToBeSwappedColour = "green"
+  const defaultColour = "#89cff0"
+  const currentItemColour = "#b1546b"
+  const itemToBeSwappedColour = "#35b5ac"
 
   const generateBtn = document.getElementById("generate-btn")
   const bubbleSortBtn = document.getElementById("bubble-sort-btn")
@@ -41,13 +41,13 @@ function App() {
   
   const generateRandData = () => {
 
-    setSorted(false)
+    setIsSorted(false)
     
     const randData = []
 
-    for(let i = 0; i < 50; i++) {
+    for(let i = 0; i < 30; i++) {
 
-      const randInt = Math.floor((Math.random() * 50) + 1)
+      const randInt = Math.floor((Math.random() * 30) + 1)
       const dataPiece = {value: randInt, colour: defaultColour}
 
       randData.push(dataPiece)
@@ -58,53 +58,56 @@ function App() {
 
   const bubbleSort = async (data) => {
 
-    disableButtons()
+    if(!isSorted) {
+      disableButtons()
     
-    let arrayLength = data.length - 1
-    let sorted = false
-    
-    while(!sorted) {
-        
-      sorted = true
-
-      for(let i = 0; i < arrayLength; i++) {
-
-        data[i].colour = currentItemColour
-        setData([...data])
-
-        if(data[i].value > data[i + 1].value) {
+      let arrayLength = data.length - 1
+      let sorted = false
+      
+      while(!sorted) {
           
-          const temp = data[i]
-          data[i] = data[i + 1]
-          data[i + 1] = temp
+        sorted = true
+
+        for(let i = 0; i < arrayLength; i++) {
+
+          data[i].colour = itemToBeSwappedColour
+          
+          setData([...data])
+          await wait(60)
+
+          if(data[i].value > data[i + 1].value) {
             
-          sorted = false
+            const temp = data[i]
+            data[i] = data[i + 1]
+            data[i + 1] = temp
+              
+            sorted = false
+
+            setData([...data])
+            await wait(60)
+          }
+
+          data[i].colour = defaultColour
+          data[i + 1].colour = defaultColour
 
           setData([...data])
-          await wait(50)
         }
 
-        data[i].colour = defaultColour
-        data[i + 1].colour = defaultColour
-
-        setData([...data])
+        arrayLength--
       }
-
-      arrayLength--
     }
 
-    setSorted(true)
+    setIsSorted(true)
     enableButtons()
   }
 
   const selectionSort = async (data) => {
 
-    if(!sorted) {
-
+    if(!isSorted) {
+      const waitTime = 200
       disableButtons()
 
       for(let i = 0; i < data.length; i++) {
-
         let lowestValueIndex = i
         data[lowestValueIndex].colour = currentItemColour
   
@@ -112,33 +115,73 @@ function App() {
   
           if(data[j].value < data[lowestValueIndex].value) {
             lowestValueIndex = j
+            data[j].colour = itemToBeSwappedColour
+
+            setData([...data])
+            await wait(waitTime)
+
+            data[j].colour = defaultColour
+            setData([...data])
           }
         }
 
         data[lowestValueIndex].colour = itemToBeSwappedColour
 
         setData([...data])
-        await wait(300)
+        await wait(waitTime)
   
         const temp = data[lowestValueIndex]
         data[lowestValueIndex] = data[i]
         data[i] = temp
   
         setData([...data])
-        await wait(300)
+        await wait(waitTime)
 
         data[i].colour = defaultColour
         data[lowestValueIndex].colour = defaultColour
       }
     }
 
-    setSorted(true)
+    setIsSorted(true)
+    enableButtons()
+  }
+
+  const insertionSort = async (data) => {
+
+    if(!isSorted) {
+      disableButtons()
+
+      for(let i = 1; i < data.length; i++) {
+        const itemToInsert = data[i]
+        let j = i - 1
+
+        itemToInsert.colour = itemToBeSwappedColour
+
+        setData([...data])
+        await wait(300)
+
+        while(j >= 0 && data[j].value > itemToInsert.value) {
+          data[j + 1] = data[j]
+          j -= 1
+        }
+
+        data[j + 1] = itemToInsert
+        
+        setData([...data])
+        await wait(300)
+
+        data[j + 1].colour = defaultColour
+        setData([...data])
+      }
+    }
+
+    setIsSorted(true)
     enableButtons()
   }
   
   return (
     <>
-      <Content data={data} generateData={generateRandData} bubbleSort={bubbleSort} selectionSort={selectionSort} />
+      <Content data={data} generateData={generateRandData} bubbleSort={bubbleSort} selectionSort={selectionSort} insertionSort={insertionSort} />
     </>
   )
 }
